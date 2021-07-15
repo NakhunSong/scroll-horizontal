3128a59dd6fd543476d37c106370ce812f2add08
Nakhun Song <nak4627@gmail.com>
2021-07-16 00:27:57 +0900

    fix: mouseup event not firing when mouse's out from the target

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (function() {
   const array = Array.from({ length: 20 }).map((e, index) => index + 1)
   array.forEach((e) => {
-    const $item = document.createElement('span')
+    const $item = document.createElement('div')
     const $itemWrapper = document.createElement('div')
     const $wrapperInner = document.querySelector('.wrapper_inner')
     $item.innerText = `item ${e}`
@@ -9,25 +9,33 @@
     $itemWrapper.setAttribute('class', 'item_wrapper')
     $itemWrapper.append($item)
     $wrapperInner.append($itemWrapper)
-    $wrapperInner.onmousedown = function(event) {
+    $itemWrapper.onmousedown = function(event) {
       console.log('mouse down!')
+
       let shiftX = event.clientX - $wrapperInner.getBoundingClientRect().left
 
       function moveAt(pageX) {
         $wrapperInner.style.transform = `translateX(${pageX - shiftX + 'px'})`
       }
       
-      function onMouseMove(event) {
-        moveAt(event.pageX)
+      function onMouseMove(e) {
+        moveAt(e.pageX)
       }
 
       moveAt(event.pageX)
 
-      document.addEventListener('mousemove', onMouseMove)
-      $wrapperInner.onmouseup = function() {
+      $itemWrapper.addEventListener('mousemove', onMouseMove)
+      $itemWrapper.onmouseout = function() {
+        console.log('mouse out!')
+        $itemWrapper.removeEventListener('mousemove', onMouseMove)
+        $itemWrapper.onmouseout = null
+        $itemWrapper.onmouseup = null
+      }
+      $itemWrapper.onmouseup = function() {
         console.log('mouse up!')
-        document.removeEventListener('mousemove', onMouseMove)
-        $wrapperInner.onmouseup = null
+        $itemWrapper.removeEventListener('mousemove', onMouseMove)
+        $itemWrapper.onmouseup = null
+        $itemWrapper.onmouseout = null
       }
     }
     $wrapperInner.ondragstart = function() {
