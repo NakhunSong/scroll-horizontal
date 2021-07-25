365aa3ae595f5bd77a8d25a9feb0bf4b337c2f6b
Nakhun Song <nak4627@gmail.com>
2021-07-25 14:38:17 +0900

    update: let the item which is closest to center of screen move to center

--- script.js
+++ script.js
@@ -1,19 +1,63 @@
+let scrollLeft
+let startX
+const $target = document.querySelector('.target')
 const $wrapperInner = document.querySelector('.wrapper_inner')
-let scrollLeft;
-let startX;
+const { left: targetLeft = 0, right: targetRight = 0 } = $target.getClientRects()[0]
 const ITEMS = Array.from({ length: 20 }).map((e, index) => index + 1)
-ITEMS.forEach((e) => {
+ITEMS.forEach((e, index) => {
   const $item = document.createElement('div')
   const $itemWrapper = document.createElement('div')
   $item.innerText = `item ${e}`
   $item.setAttribute('class', 'item')
   $itemWrapper.setAttribute('class', 'item_wrapper')
+  if (index === 0) {
+    $itemWrapper.classList.add('active')
+  }
   $itemWrapper.append($item)
   $wrapperInner.append($itemWrapper)
 })
 
+function scrollToHorizontal(walk) {
+  if (!$wrapperInner) return
+  $wrapperInner.scrollTo({
+    behavior: 'smooth',
+    left: walk,
+  })
+}
+
+function moveCenter(focusedItem) {
+  const $itemWrappers = document.querySelectorAll('.item_wrapper')
+  const firstItemLeft = $itemWrappers[0].getClientRects()[0].left
+  const walk = Math.round(focusedItem.getClientRects()[0].left - firstItemLeft)
+  scrollToHorizontal(walk)
+  focusedItem.classList.add('active')
+}
+
+function getClosest() {
+  const $itemWrappers = document.querySelectorAll('.item_wrapper')
+  let leftItem
+  let rightItem
+  let leftCollapse = 0
+  let rightCollapse = 0
+  $itemWrappers.forEach((item) =>  {
+    item.classList.remove('active')
+    const { left, right } = item.getClientRects()[0]
+    if (left < targetLeft && targetLeft < right) {
+      leftItem = item
+      leftCollapse = Math.abs(right - targetLeft)
+    } else if (left < targetRight && targetRight < right) {
+      rightItem = item
+      rightCollapse = Math.abs(left - targetRight)
+    }
+  })
+  if (leftCollapse > rightCollapse) {
+    return leftItem
+  } else {
+    return rightItem
+  }
+}
+
 function handleMouseMove(event) {
-  console.log('mouse move!')
   event.preventDefault()
   const x = event.pageX - $wrapperInner.offsetLeft
   const walk = (x - startX) * 2
@@ -22,24 +66,23 @@ function handleMouseMove(event) {
 }
 
 $wrapperInner.onmousedown = function(event) {
-  console.log('mouse down!')
   $wrapperInner.classList.add('grab')
   startX = event.pageX - $wrapperInner.offsetLeft
   scrollLeft = $wrapperInner.scrollLeft
   
   $wrapperInner.onmousemove = handleMouseMove
   $wrapperInner.onmouseleave = function() {
-    console.log('mouse leave!')
     $wrapperInner.classList.remove('grab')
     $wrapperInner.onmousemove = null
     $wrapperInner.onmouseup = null
     $wrapperInner.onmouseleave = null
   }
   $wrapperInner.onmouseup = function() {
-    console.log('mouse up!')
     $wrapperInner.classList.remove('grab')
     $wrapperInner.onmousemove = null
     $wrapperInner.onmouseup = null
     $wrapperInner.onmouseleave = null
+    const focusedItem = getClosest()
+    moveCenter(focusedItem)
   }
 }
